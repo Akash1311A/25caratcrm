b1d0fcb85b239e51fd3d7b99ed8a3960934052f1
Akash1311A <akashkjmsgupta@gmail.com>
2026-04-08 14:47:05 +0530

    Always run migrations on startup

--- 25caratcrm/wsgi.py
+++ 25caratcrm/wsgi.py
@@ -7,15 +7,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "25caratcrm.settings")
 
 
-def _should_run_migrations():
-    value = os.environ.get("RUN_MIGRATIONS_ON_STARTUP")
-    if value is None:
-        value = "true" if os.environ.get("RENDER") == "true" else "false"
-    return value.lower() in {"1", "true", "yes", "on"}
-
-
-if _should_run_migrations():
-    django.setup()
-    call_command("migrate", interactive=False, verbosity=0)
+django.setup()
+call_command("migrate", interactive=False, verbosity=0)
 
 application = get_wsgi_application()
