42d325b501a66732cc081300c6ffa34abb1c97a6
Akash1311A <akashkjmsgupta@gmail.com>
2026-04-08 15:58:11 +0530

    Simplify admin registrations for production

--- crm/admin.py
+++ crm/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import GroupAdmin as DjangoGroupAdmin
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
-from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from django.contrib.auth.models import Group, User
 from .models import Customer, FollowUp, Purchase, ProductType
 
@@ -30,26 +29,12 @@ class FollowUpInline(admin.TabularInline):
 
 @admin.register(User)
 class CustomUserAdmin(DjangoUserAdmin):
-    add_form = UserCreationForm
-    form = UserChangeForm
     model = User
     list_display = ("username", "email", "first_name", "is_staff", "is_superuser", "is_active", "last_login")
     list_filter = ("is_staff", "is_superuser", "is_active", "groups")
     search_fields = ("username", "first_name", "last_name", "email")
     ordering = ("username",)
     filter_horizontal = ("groups", "user_permissions")
-    fieldsets = (
-        (None, {"fields": ("username", "password")}),
-        ("Personal info", {"fields": ("first_name", "last_name", "email")}),
-        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
-        ("Important dates", {"fields": ("last_login", "date_joined")}),
-    )
-    add_fieldsets = (
-        (None, {
-            "classes": ("wide",),
-            "fields": ("username", "email", "first_name", "last_name", "password1", "password2", "is_staff", "is_superuser"),
-        }),
-    )
 
 
 @admin.register(Group)
@@ -114,7 +99,6 @@ class PurchaseAdmin(admin.ModelAdmin):
     date_hierarchy = "date"
     list_select_related = ("customer", "assigned_to")
     list_per_page = 30
-    autocomplete_fields = ("customer", "assigned_to", "products")
     filter_horizontal = ("products",)
 
     def get_queryset(self, request):
