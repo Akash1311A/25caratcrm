76309777c8a07304a573b914ca32d557223bbd5e
Akash1311A <akashkjmsgupta@gmail.com>
2026-04-10 14:25:46 +0530

    Refresh branding and restore static serving

--- jaishreefashioncrm/wsgi.py
+++ jaishreefashioncrm/wsgi.py
@@ -1,7 +1,13 @@
 import os
 
+import django
+from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jaishreefashioncrm.settings")
+
+if os.environ.get("RUN_STARTUP_TASKS", "true").lower() in {"1", "true", "yes", "on"}:
+    django.setup()
+    call_command("collectstatic", interactive=False, verbosity=0, clear=False)
 
 application = get_wsgi_application()
