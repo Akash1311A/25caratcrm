1df95a9db29bec587f6725942974efff787dfadb
Akash1311A <akashkjmsgupta@gmail.com>
2026-04-30 22:58:01 +0530

    Add static file fallback route

--- jaishreefashioncrm/urls.py
+++ jaishreefashioncrm/urls.py
@@ -1,9 +1,16 @@
+from django.conf import settings
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.views.generic import RedirectView
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("crm.urls")),
     path("favicon.ico", RedirectView.as_view(url="/static/crm/favicon.ico")),
+    re_path(
+        r"^static/(?P<path>.*)$",
+        serve,
+        {"document_root": settings.BASE_DIR / "crm" / "static"},
+    ),
 ]
